07111d21e7d7b7e11e39430839d5f232e5744a04
Japjot Singh <japjotsingh0802@gmail.com>
2026-03-03 16:19:33 -0500

    Wire getting-started and p2 pages with Header

--- gettingStarted.jsx
+++ gettingStarted.jsx
@@ -1,10 +1,22 @@
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.css';
 
+import Header from './components/header/header';
 import Example from './components/example/Example';
 
+function App() {
+  return (
+    <div>
+      <Header />
+      <Example />
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <Example />,
+  <App />,
   document.getElementById('reactapp'),
 );
--- p2.jsx
+++ p2.jsx
@@ -1,9 +1,22 @@
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/main.css';
 
+import Header from './components/header/header';
 import States from './components/states/States';
 
+function App() {
+  return (
+    <div>
+      <Header />
+      <States />
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <States />,
+  <App />,
   document.getElementById('reactapp'),
 );
